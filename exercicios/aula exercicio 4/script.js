650c65e74aec2810056d24bf66f5c58da783430e
Felipe Tahara <felipehideakit@gmail.com>
2025-12-15 17:03:47 -0300

    exercício JS

--- exercicios/aula exercicio 4/script.js
+++ exercicios/aula exercicio 4/script.js
@@ -1,20 +1,18 @@
-function carregar() {
-    var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('imagem')
-    var data = new Date()
-    var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas.`
-    if (hora >= 0 && hora < 12) {
-        //BOM DIA!
-        img.src = 'fotomanha.png'
-        document.body.style.background = '#ffbf758f'
-    } else if (hora >= 12 && hora <= 18) {
-        //BOA TARDE!
-        img.src = 'fototarde.png'
-        document.body.style.background = '#9f480ad5'
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número!')
     } else {
-        //BOA NOITE!
-        img.src = 'fotonoite.png'
-        document.body.style.background = '#31444c'
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ' '
+        while (c <= 15) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
     }
 }
