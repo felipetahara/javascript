22e3129a5732a94c31e381f9f3bccaa080e7e3f4
Felipe Tahara <felipehideakit@gmail.com>
2025-12-17 12:50:04 -0300

    Aprendendo sobre variáveis compostas

---
+++ exercicios/ex014/ambiente.js
@@ -0,0 +1,11 @@
+let num = [5, 8 , 9, 3, 12, 122]
+num.sort()
+console.log(num)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+console.log(`O vetor tem ${num.length} posições`)
+let pos = num.indexOf(4)
+if (pos == -1) {
+    console.log(`O valor não foi encontrado!`)
+} else {
+    console.log(`O valor 8 está na posição ${pos}`)
+}
